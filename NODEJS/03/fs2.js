9134e6c27463c1bab349fcb430d9c04489424e86
Nuno Marques <nuno.marques@mmlimited.pt>
2026-07-03 21:46:00 +0100

    fs

--- NODEJS/03/fs2.js
+++ NODEJS/03/fs2.js
@@ -8,8 +8,9 @@ console.log(filePath);
 const stats = fs.statSync(filePath);
 
 if (stats.isFile()){
-    var content = fs.readFileSync(filePath, 'utf-8');
-    console.log(content)
+    fs.readFile(filePath, 'utf-8', (error, contents) =>{
+        console.log(contents);
+    });
 }
 console.log('Cheguei ao fim do codigo!');
 
